cac1ab17a29b2ac4245d17b924977d4e8bdabc8e
salsabeel <123001325+Salsa-beel@users.noreply.github.com>
2023-09-06 08:54:06 +0300

    some changes in json file

--- src/app/shared/components/filtering/filtering.component.ts
+++ src/app/shared/components/filtering/filtering.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../service/shared.service';
 
 @Component({
   selector: 'app-filtering',
   templateUrl: './filtering.component.html',
   styleUrls: ['./filtering.component.css']
 })
-export class FilteringComponent {
+export class FilteringComponent implements OnInit {
 
+  categories:any[]=[];
+
+  constructor(private service:SharedService){}
+
+  ngOnInit(): void{
+    this.getCategories();
+
+  }
+
+  getCategories(){
+
+    this.service.getAllCategories().subscribe((res:any) =>{
+      this.categories=res
+      // console.log(res)
+    })
+  }
+
+
+  filterCategory(event:any){
+    let value = event.target.value;
+   this.getRecipeCategory(value)
+  }
+
+  getRecipeCategory(keyword:string){
+this.service.getRecipeByCategory(keyword).subscribe(res=>{
+
+
+})
+
+  }
 }
--- src/app/shared/service/shared.service.ts
+++ src/app/shared/service/shared.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class SharedService {
 
-  constructor() { }
+  constructor( private http:HttpClient) { }
+
+getAllCategories(){
+
+  return this.http.get('http://localhost:3000/categories')
 }
+
+getRecipeByCategory(keyword:string){
+
+  return this.http.get('http://localhost:3000/categories/'+keyword)
+}
+
+}
+
