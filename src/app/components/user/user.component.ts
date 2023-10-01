7f9f3374686ea7eeee0c0827630b8d5122844cc3
salsabeel <123001325+Salsa-beel@users.noreply.github.com>
2023-10-01 18:19:50 +0300

    added login service

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { FormGroup,FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/recipes-service/services/auth.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  public loginForm!:FormGroup
+
+  constructor(private authService :AuthService , private formBuilder:FormBuilder , private router:Router) { }
 
   ngOnInit() {
+
+    this.loginForm=this.formBuilder.group(
+      {
+        email:[''],
+        password:['']
+      }
+
+
+    )
   }
 
+  // onLogin(loginForm: { value: any; }){
+  //   console.log(loginForm.value)
+
+  //   const token = this.authService.authUSer(loginForm.value);
+
+  //   if (token){
+
+  //    localStorage.setItem('token',token.userName)
+  //    alert('login successful');
+  //   } else {
+
+  //    alert('login not successful');
+  //    }
+  //   }
+
+login(){
+
+  this.authService.loginUser().subscribe( res=> {
+    const user =  res.find((a:any)=>{
+      return a.email ===  this.loginForm.value.email && a.password === this.loginForm.value.password
+    });
+
+    if (user){
+      alert('تم الدخول بنجاح');
+      this.loginForm.reset();
+      this.router.navigate(['home'])
+    } else {
+      alert('ليس لديك حساب')
+    }
+
+  },error=>{
+    alert('حدث خطأ')
+  }
+
+  )
 }
+
+
+  }
+
